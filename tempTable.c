67fbb36262e176f760fa569d9222220a38409abe
Gabrielle Rife <43075585+gaberife@users.noreply.github.com>
2019-01-10 10:51:35 -0500

    Update tempTable.c

--- tempTable.c
+++ tempTable.c
@@ -8,10 +8,12 @@ int tableRun()
  double temp = 0;
  double fahr = 0;
  double cel = 0;
- double final = 0;
+ double final = 0
+  
 /* Prompt the user to enter starting Celcius Temperature */
 printf("Enter starting Celsius temperature: \n");
 scanf("%lf",&cel);
+ 
 /* Prompt the user to enter ending Celcius Temperature */
 printf("Enter ending  Celsius temperature: \n");
 scanf("%lf",&final);
@@ -26,7 +28,6 @@ scanf("%d",&step);
  while(cel <= final){
   fahr  = cTof(cel);
   printf("\n%3.2f\t |  %3.2f", cel, fahr);
-  // = fToc(temp);                                                              
   cel += step;
   }
  printf("\n");
